9bb0e6a883bfc0e3b68b66ab2b8cbbdc17e9cf3c
Dominic Russo <domski345@gmail.com>
2023-04-18 17:01:24 -0400

    certified python moment

--- proxy.py
+++ proxy.py
@@ -75,13 +75,10 @@ def cable():
 
     # Make API call to update the VM's name in GNS3
     api_url = f"http://gns3.brownout.tech:3080/v2/projects/{project_id}/links"
-    data = {"nodes": [{ "node_id": a_id, "adapter_number": a_label, "port_number": 0 }, { "node_id": b_id, "adapter_number": b_label, "port_number": 0 }]}
-    print(data)
+    data = {"nodes": [{ "node_id": a_id, "adapter_number": int(a_label), "port_number": 0 }, { "node_id": b_id, "adapter_number": int(b_label), "port_number": 0 }]}
     response = requests.put(api_url, json=data)
 
     # Extract GNS3 assigned data
-    print(response)
-
     link_id = response.json()["link_id"]
 
     # Update netbox with the cable ID
