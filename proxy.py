795b88f863f5d428c719b04574b0eac48cb61f9e
Dominic Russo <domski345@gmail.com>
2023-05-04 21:25:59 -0400

    code cleanup

--- proxy.py
+++ proxy.py
@@ -11,8 +11,6 @@ nb = pynetbox.api('http://netbox.brownout.tech:8000/', token=netbox_token)
 
 @application.post("/device")
 def device():
-    print("Yo Dawg, I heard you like routers?") # Sarcastic remark
-
     #Error checking
     if not request.is_json:
         return {"error": "Request must be JSON"}, 415
@@ -38,23 +36,34 @@ def device():
     mac_address = "00:20:91:%02x:%02x:%02x" % (random.randint(0, 255),random.randint(0, 255),random.randint(0, 255))
     options = f"-nic bridge,br=br0,model=e1000,mac={mac_address}"
 
-    # Make API call to update the VM's name in GNS3
+    # Make API call to update the VM's name and Mgmt nic in GNS3
     api_url = f"http://gns3.brownout.tech:3080/v2/projects/{project_id}/nodes/{node_id}"
     data = {"name": name, "properties": { "options": options } }
     response = requests.put(api_url, json=data)
 
-    # Update netbox to reflect node_id change and status
+    # Update netbox console port and node_id
     nb.dcim.devices.update([{'id': id, 'serial': node_id, 'asset_tag': console}])
 
     # Begin ZTP
     api_url = f"http://gns3.brownout.tech:3080/v2/projects/{project_id}/nodes/{node_id}/start"
     requests.post(api_url)
 
+    # Allocate a mgmt ip address
     primary_ip6 = nb.ipam.prefixes.get(2).available_ips.create() # 2 is the Mgmt prefix
+
+    # Find mgmt interface ID
     int_id = nb.dcim.interfaces.get(device_id=id,name="MgmtEth0/0/CPU0/0")['id']
+
+    # Assign mgmt IP to the mgmt interface
     nb.ipam.ip_addresses.update([{'id': primary_ip6.id, 'vrf': 1, 'assigned_object_type': 'dcim.interface', 'assigned_object_id': int_id}])
+
+    # Set the mgmt interface vrf
     nb.dcim.interfaces.update([{'id': int_id, 'vrf': 1}])
+
+    # set the status and primary ip of the device 
     nb.dcim.devices.update([{'id': id, 'status': "planned", 'primary_ip6': primary_ip6.id}])
+
+    # Call the "ZTP" telnet script
     device_args=[console,name,primary_ip6,id]
     configure_thread = threading.Thread(target=configure, name="configure_device", args=device_args)
     configure_thread.start()
@@ -64,14 +73,23 @@ def device():
 
 @application.delete("/device")
 def device_delete():
+    #Error checking
     if not request.is_json:
         return {"error": "Request must be JSON"}, 415
+    
+    # Find the device's node_id from the call's json body
     device = request.get_json()
     node_id = device['data']['serial']
+
+    # Turn off th router
     requests.post(f"http://gns3.brownout.tech:3080/v2/projects/{project_id}/nodes/{device['data']['serial']}/stop")
     name = device['data']['name']
+
+    # Delete the router
     api_url = f"http://gns3.brownout.tech:3080/v2/projects/{project_id}/nodes/{node_id}"
     requests.delete(api_url)
+
+    # Happy return code back to netbox
     return f"{name} was deleted", 201
 
 @application.post("/cable")
@@ -84,45 +102,40 @@ def cable():
     
     cable = request.get_json()
 
-    id = cable['data']['id']
-    a_interface_id = cable['data']['a_terminations'][0]['object_id']
-    b_interface_id = cable['data']['b_terminations'][0]['object_id']
-
     # Get necessary data from netbox
     device_a = nb.dcim.devices.get(id=cable['data']['a_terminations'][0]['object']['device']['id'])
     device_b = nb.dcim.devices.get(id=cable['data']['b_terminations'][0]['object']['device']['id'])
-    interface_a = nb.dcim.interfaces.get(id=a_interface_id)
-    interface_b = nb.dcim.interfaces.get(id=b_interface_id)
+    interface_a = nb.dcim.interfaces.get(id=cable['data']['a_terminations'][0]['object_id'])
+    interface_b = nb.dcim.interfaces.get(id=cable['data']['b_terminations'][0]['object_id'])
 
     # Make API call to create the cable in GNS3
     api_url = f"http://gns3.brownout.tech:3080/v2/projects/{project_id}/links"
     data = {"nodes": [{ "node_id": device_a['serial'], "adapter_number": int(interface_a['label']), "port_number": 0 }, { "node_id": device_b['serial'], "adapter_number": int(interface_b['label']), "port_number": 0 }]}
     response = requests.post(api_url, json=data)
 
-    # Extract GNS3 assigned data
-    link_id = response.json()["link_id"]
-
     # Control logic for determining the link type
     if device_a['device_role']['id'] == 3:
         # If a is a core..
         if device_b['device_role']['id'] == 3:
-            # core to core, do is-is
-            nb.dcim.interfaces.update([{'id': a_interface_id, 'vrf': 2, 'tags': [{'id': 1}]}])
-            nb.dcim.interfaces.update([{'id': b_interface_id, 'vrf': 2, 'tags': [{'id': 1}]}])
+            # core to core, do is-is (tag 1 is is-is)
+            nb.dcim.interfaces.update([{'id': cable['data']['a_terminations'][0]['object_id'], 'vrf': 2, 'tags': [{'id': 1}]}])
+            nb.dcim.interfaces.update([{'id': cable['data']['b_terminations'][0]['object_id'], 'vrf': 2, 'tags': [{'id': 1}]}])
 
-            # generate v6 prefix for link
+            # generate v6 prefix for link, 6 is the prefix to allocate /127s from. should be chosen dynamically
             prefix = nb.ipam.prefixes.get(6).available_prefixes.create({"prefix_length": 127})
             # generate ip addresses from prefix
             ip_a_side = prefix.available_ips.create()
             ip_b_side = prefix.available_ips.create()
 
             # push ip address changes to Netbox
-            nb.ipam.ip_addresses.update([{'id': ip_a_side.id, 'assigned_object_type': 'dcim.interface', 'assigned_object_id': a_interface_id}])
-            nb.ipam.ip_addresses.update([{'id': ip_b_side.id, 'assigned_object_type': 'dcim.interface', 'assigned_object_id': b_interface_id}])
+            nb.ipam.ip_addresses.update([{'id': ip_a_side.id, 'assigned_object_type': 'dcim.interface', 'assigned_object_id': cable['data']['a_terminations'][0]['object_id']}])
+            nb.ipam.ip_addresses.update([{'id': ip_b_side.id, 'assigned_object_type': 'dcim.interface', 'assigned_object_id': cable['data']['b_terminations'][0]['object_id']}])
 
     # Update netbox with the cable ID
-    nb.dcim.cables.update([{'id': id, 'label': link_id}])
-    return f"Cable: {link_id} was created", 201
+    nb.dcim.cables.update([{'id': cable['data']['id'], 'label': response.json()["link_id"]}])
+
+    # Happy return code back to netbox
+    return f"", 201
 
 
 @application.delete("/cable")
@@ -137,17 +150,17 @@ def cable_delete():
 
 @application.patch("/device")
 def device_update():
-    print("Oh no") # Sarcastic remark
-
     #Error checking
     if not request.is_json:
         return {"error": "Request must be JSON"}, 415
-    
     update = request.get_json()
+
+    # Only re-apply the config if the status is set to staged
     if update['data']['status']['value'] == 'staged':
         # POST request to render config from NetBox
         api_url = f"http://netbox.brownout.tech:8000/api/dcim/devices/{update['data']['id']}/render-config/"
         response = requests.post(api_url, headers={'authorization' : f'Token {netbox_token}'})
+        
         # Push config using NAPALM to device
         mgmt_ip = ipaddress.IPv6Interface(update['data']['primary_ip6']['address']).ip
         device_driver = get_network_driver("iosxr")
@@ -157,6 +170,8 @@ def device_update():
         device.commit_config()
         device.close()
         nb.dcim.devices.update([{'id': update['data']['id'], 'status': "active"}])
+
+    # Happy return code back to netbox
     return f"{update['data']['name']} is being configured", 201
 
 # Debug
